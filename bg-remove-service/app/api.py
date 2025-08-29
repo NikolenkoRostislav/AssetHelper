41913438e08e6989013284630cd30f86358cd047
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-08-29 10:04:46 +0200

    feat: added background removal for one and multiple images

--- bg-remove-service/app/api.py
+++ bg-remove-service/app/api.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile
 from app.service import BgRemoveService
 from app.utils.exceptions import handle_exceptions
 
 router = APIRouter(prefix="/bg-rm", tags=["bg-rm"])
 
-@router.post("/")
+@router.post("/image")
 @handle_exceptions
-async def bg_rm(image: str):
+async def bg_rm(image: UploadFile):
     return await BgRemoveService.remove_bg(image)
+
+
+@router.post("/images")
+@handle_exceptions
+async def bg_rm(images: list[UploadFile]):
+    return await BgRemoveService.remove_bgs(images)
--- bg-remove-service/app/service.py
+++ bg-remove-service/app/service.py
@@ -1,6 +1,50 @@
+from io import BytesIO
+from zipfile import ZipFile
+from fastapi import UploadFile
+from fastapi.responses import StreamingResponse
+from rembg import remove
+from PIL import Image
 from app.utils.exceptions import *
 
 class BgRemoveService:
     @staticmethod
-    async def remove_bg(image: str):
-        return {"detail": f"removed background from {image}" }
+    async def remove_bg(image: UploadFile):
+        contents = await image.read()
+        try:
+            input_image = Image.open(BytesIO(contents))
+        except Exception:
+            raise InvalidEntryError("Something failed")
+
+        output_image = remove(input_image)
+
+        buffer = BytesIO()
+        output_image.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        return StreamingResponse(buffer, media_type="image/png", headers={
+            "Content-Disposition": 'attachment; filename="updated_image.png"'
+        })
+
+
+    @staticmethod
+    async def remove_bgs(images: list[UploadFile]):
+        zip_buffer = BytesIO()
+        with ZipFile(zip_buffer, "w") as zf:
+            for image in images:
+                contents = await image.read()
+                try:
+                    input_image = Image.open(BytesIO(contents))
+                except Exception:
+                    raise InvalidEntryError("Something failed")
+
+                output_image = remove(input_image)
+
+                img_buffer = BytesIO()
+                output_image.save(img_buffer, format="PNG")
+                img_buffer.seek(0)
+
+                zf.writestr(image.filename, img_buffer.read())
+        zip_buffer.seek(0)
+        return StreamingResponse(zip_buffer, media_type="application/zip", headers={
+            "Content-Disposition": 'attachment; filename="images.zip"'
+        })
