e43a5b90ce28547b830bab72ab24e03aa966983f
NikolenkoRostislav <rostislavnikolenkowork@gmail.com>
2025-09-23 15:27:32 +0200

    feat: setup pytest

---
+++ mp4-to-mp3-service/tests/test_misc.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+
+def func(num: int = 2) -> int:
+    return 4/num
+
+def test_func():
+    assert func() == 2
+    assert func(3) != 2
+    assert func(2) == 2
+    try:
+        func(0)
+    except ZeroDivisionError:
+        assert True
+    else:
+        assert False
+
